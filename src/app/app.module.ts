84a541536924435ffb793cb33295e96c07b8238e
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-23 14:38:33 +0300

    Add all required componets to start the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    NavbarComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
