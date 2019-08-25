684a9b9a868cd32c52784e0c147ca216f47fbf0d
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-08-25 21:56:37 +0300

    Remove unused directive

--- src/app/gitdirective.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GitdirectiveDirective } from './gitdirective.directive';
-
-describe('GitdirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new GitdirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
